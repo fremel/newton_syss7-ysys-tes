51855cbd51749b3291e6ec1a78c36778efb0bf0f
FredrikMellgren <fredrik.mellgren@martinservera.se>
2025-03-23 08:58:16 +0100

    Added Exercise_15b.

---
+++ Exercise_15b/DiscountCalculator/DiscountCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace DiscountCalculator;
+
+public class DiscountCalculator
+{
+    public double CalculateDiscount(double orderAmount, bool isPremiumMember)
+    {
+        double discount = 0.0;
+        if (orderAmount > 100)
+        {
+            discount = 10; // 10% discount for orders over $100
+            if (isPremiumMember)
+            {
+                discount += 5; // Additional 5% discount for premium members
+            }
+        }
+        else if (isPremiumMember)
+        {
+            discount = 5; // 5% discount for premium members on orders $100 or below
+        }
+        return discount;
+    }
+}
